e37b88ab19864112f1d6c406dc57b62a1978c9fe
Sam Glerum <samuelglerum@hotmail.com>
2018-05-14 20:16:25 +0200

    created auth_routes for login and registration api_calls

--- server.js
+++ server.js
@@ -9,53 +9,8 @@ let port = process.env.PORT || config.webPort;
 server.use(bodyparser.urlencoded({
     extended: true
 }));
-//
-// server.use(bodyparser.json());
 
-/* Login and Registration */
-
-// Login endpoint
-server.post('/api/login', (req, res, next) => {
-    let loginParameters = req.body;
-
-
-    let query = {
-        sql: 'SELECT Email, Password FROM user',
-    };
-
-
-    db.query(query, (error, rows, fields) => {
-       if (error) {
-           res.status(400);
-           res.json(error);
-       } else {
-           res.status(200);
-           res.json(rows);
-       }
-    });
-});
-
-//Registration endpoint
-server.post('/api/register', (req, res, next) => {
-    let user = req.body;
-    let query = {
-        sql: 'INSERT INTO user (Voornaam, Achternaam, Email, Password) VALUES (?, ?, ?, ?)',
-        values: [user.firstname, user.lastname, user.email, user.password],
-        timeout: 2000
-    };
-    console.log('User query: ' + query.sql);
-
-    res.contentType('application/json');
-    db.query(query, (error, rows, fields) => {
-        if (error) {
-            res.status(400);
-            res.json(error);
-        } else {
-            res.status(200);
-            res.json(rows);
-        }
-    });
-});
+server.use('/api', require('./routes/auth_routes'));
 
 server.get('*', (req, res) => {
     res.status(404);
