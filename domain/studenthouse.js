9357ec8170fdc93147e1bc2dbbaa4cfbddec82eb
Arjan Gosens <arjangosens@hotmail.com>
2018-05-14 13:21:50 +0200

    Added getters and setters for variables in studenthouse.js

--- domain/studenthouse.js
+++ domain/studenthouse.js
@@ -1,12 +1,69 @@
 class StudentHouse {
     constructor(name, street, houseNumber, postCode, city, userId) {
-        this.id = id;
-        this.name = name;
-        this.street = street;
-        this.houseNumber = houseNumber;
-        this.postCode = postCode;
-        this.city = city;
-        this.userId = userId;
+        this._id = id;
         this.owner = userId;
+        this._name = name;
+        this._street = street;
+        this._houseNumber = houseNumber;
+        this._postCode = postCode;
+        this._city = city;
+        this._userId = userId;
+    }
+
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get street() {
+        return this._street;
+    }
+
+    set street(value) {
+        this._street = value;
+    }
+
+    get houseNumber() {
+        return this._houseNumber;
+    }
+
+    set houseNumber(value) {
+        this._houseNumber = value;
+    }
+
+    get postCode() {
+        return this._postCode;
+    }
+
+    set postCode(value) {
+        this._postCode = value;
+    }
+
+    get city() {
+        return this._city;
+    }
+
+    set city(value) {
+        this._city = value;
+    }
+
+    get userId() {
+        return this._userId;
+    }
+
+    set userId(value) {
+        this._userId = value;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
     }
 }
