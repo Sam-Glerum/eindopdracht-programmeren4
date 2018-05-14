db80289dd21fd8012017998a3d91c277bd0ee853
Sam Glerum <samuelglerum@hotmail.com>
2018-05-14 13:41:20 +0200

    removed authentication.js as it was not necessary. Moved the endpoints for authentication and login to server.js

--- server.js
+++ server.js
@@ -3,3 +3,24 @@ let server = express();
 
 let port = process.env.PORT || config.webPort;
 
+
+// Login endpoint
+server.get('/login', (req, res) => {
+    res.status(200);
+    // ToDo: Logica voor inloggen implementeren
+});
+
+// Registration endpoint
+server.get('/register', (req, res) => {
+    res.status(200);
+    // ToDo: Logica voor registreren implementeren.
+});
+
+
+
+server.get('*', (req, res) => {
+    res.status(404);
+    res.json({
+        "msg": "Requested resource not found"
+    })
+});
