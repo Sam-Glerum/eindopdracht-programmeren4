8d5519f8005797135057c6805103a48dd601ccd0
Sam Glerum <samuelglerum@hotmail.com>
2018-05-14 13:05:37 +0200

    created StudentHouse object, added attributes

---
+++ domain/studenthouse.js
@@ -0,0 +1,11 @@
+class StudentHouse {
+    constructor(id, name, street, houseNumber, postCode, city, userId) {
+        this.id = id;
+        this.name = name;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.postCode = postCode;
+        this.city = city;
+        this.userId = userId;
+    }
+}
