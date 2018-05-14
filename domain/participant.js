6819d19ad4179a72c65c2155f5ef6af498a901c9
Arjan Gosens <arjangosens@hotmail.com>
2018-05-14 13:21:07 +0200

    Added getters and setters for variables in participant.js

--- domain/participant.js
+++ domain/participant.js
@@ -1,7 +1,32 @@
 class Participant {
     constructor(userId, studentHouseId, mealId) {
-        this.userId = userId;
-        this.studentHouseId = studentHouseId;
-        this.mealId = mealId;
+        this._userId = userId;
+        this._studentHouseId = studentHouseId;
+        this._mealId = mealId;
+    }
+
+
+    get userId() {
+        return this._userId;
+    }
+
+    set userId(value) {
+        this._userId = value;
+    }
+
+    get studentHouseId() {
+        return this._studentHouseId;
+    }
+
+    set studentHouseId(value) {
+        this._studentHouseId = value;
+    }
+
+    get mealId() {
+        return this._mealId;
+    }
+
+    set mealId(value) {
+        this._mealId = value;
     }
 }
