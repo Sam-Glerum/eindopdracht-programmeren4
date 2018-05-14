2c43c0d184a0d02e385b807eeaf48e9ccc64f068
Sam Glerum <samuelglerum@hotmail.com>
2018-05-14 13:07:31 +0200

    created meal object, added attributes

---
+++ domain/meal.js
@@ -0,0 +1,12 @@
+class Meal {
+    constructor(id, name, description, ingredients, allergy, price, userId, studentHouseId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.allergy = allergy;
+        this.price = price;
+        this.userId = userId;
+        this.studentHouseId = studentHouseId;
+    }
+}
