823ff4a929c25b8f28cb4876929a5130b7449b78
Sam Glerum <samuelglerum@hotmail.com>
2018-05-14 15:13:06 +0200

    added get-request method for getting a list of studenthouses

--- server.js
+++ server.js
@@ -1,28 +1,66 @@
 let express = require('express');
 let server = express();
+let config = require('./config/config');
+let db = require('./config/db');
 
 let port = process.env.PORT || config.webPort;
 
 /* Login and Registration */
 
 // Login endpoint
-server.post('/login', (req, res) => {
+server.post('/api/login', (req, res) => {
     let loginInfo = req.body;
-    let query = {
-        sql: ""
-    }
 
     res.status(200);
     // ToDo: Logica voor inloggen implementeren
 });
 
 // Registration endpoint
-server.post('/register', (req, res) => {
-    res.status(200);
-    // ToDo: Logica voor registreren implementeren.
-});
+// server.post('/api/register', (req, res) => {
+//     let user = req.body;
+//     let firstname = user.firstname;
+//     let query = {
+//         sql: 'INSERT INTO `user` (Voornaam, Achternaam, Email, Password) VALUES (?, ?, ?, ?)',
+//         values: [firstname, user.lastname, user.email, user.password],
+//         timeout: 2000
+//     };
+//     console.dir(user);
+//     console.log('User query: ' + query.sql);
+//
+//     res.contentType('application/json');
+//     db.query(query, (error, rows, fields) => {
+//         if (error) {
+//             res.status(400);
+//             res.json(error);
+//         } else {
+//             res.status(200);
+//             res.json(rows);
+//         }
+//     });
+// });
 
 /* StudentHouse endpoints */
+server.post('/api/studentenhuis', (req, res) => {
+    res.status(200);
+    //Todo Logica
+});
+
+server.get('/api/studentenhuis', (req, res) => {
+    res.contentType('application/json');
+
+    db.query('SELECT * FROM studentenhuis', (error, rows, fields) => {
+        if (error) {
+            res.status(400).json(error);
+        } else {
+            res.status(200).json(rows);
+        }
+    })
+});
+
+
+
+
+
 
 
 
@@ -34,3 +72,7 @@ server.get('*', (req, res) => {
         "msg": "Requested resource not found"
     })
 });
+
+server.listen(port, () => {
+    console.log("Server is listening on port " + port);
+})
