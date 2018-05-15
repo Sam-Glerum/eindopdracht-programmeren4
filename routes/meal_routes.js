bb212434875c4c3834a4ff587e51abb36a622b45
Arjan Gosens <arjangosens@hotmail.com>
2018-05-15 16:51:51 +0200

    Added GET api/studentenhuis/:shId/maaltijd
    Returns all meals connected to a certain student house

--- routes/meal_routes.js
+++ routes/meal_routes.js
@@ -10,12 +10,29 @@ router.all('/studentenhuis/maaltijd', (req, res, next) => {
     // console.log("POST request to studentenhuis/" + /**+ studentHouseId */ + "/maaltijd")
 });
 
+// Get a specific meal by it's id
 router.get('/studentenhuis/:shId/maaltijd/:maId', (req, res, next) => {
     let studentHouseId = req.params.shId;
     let mealId = req.params.maId;
     console.log("GET studentenhuis/" + studentHouseId +"/maaltijd/" + mealId);
 
     db.query('SELECT * FROM maaltijd WHERE ID = ' + mealId + ' AND StudentenhuisID = ' + studentHouseId, (error, rows, fields) => {
+        console.log(rows);
+        if (error) {
+            res.status(400).json(error);
+        } else {
+            res.status(200).json(rows);
+        }
+    });
+
+});
+
+// Get all meals that are connected to one specific student house
+router.get('/studentenhuis/:shId/maaltijd', (req, res, next) => {
+    let studentHouseId = req.params.shId;
+    console.log("GET studentenhuis/" + studentHouseId +"/maaltijd");
+
+    db.query('SELECT * FROM maaltijd WHERE StudentenhuisID = ' + studentHouseId, (error, rows, fields) => {
         console.log(rows);
         if (error) {
             res.status(400).json(error);
