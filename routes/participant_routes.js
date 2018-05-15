89dc5a85ec91383f51d5ed9219a943ffbf19263b
Sam Glerum <samuelglerum@hotmail.com>
2018-05-15 18:53:12 +0200

    added requirements

---
+++ routes/participant_routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+const auth = require('../auth/authentication');
+const config = require('../config/config');
+const authController = require('../auth/auth_controller');
+const bodyparser = require('body-parser');
+const jwt = require('jwt-simple');
+
