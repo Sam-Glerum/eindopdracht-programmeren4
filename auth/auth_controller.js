ca0b42ed12091818365eddffa720cd7f3518eb93
Sam Glerum <samuelglerum@hotmail.com>
2018-05-16 19:42:59 +0200

    removed commented code from auth_controller

--- auth/auth_controller.js
+++ auth/auth_controller.js
@@ -1,26 +1,6 @@
 const auth = require('./authentication');
 let ApiError = require('../model/ApiError');
 
-// let validateToken = (req, res, next) => {
-//     console.log('validateToken called');
-//     let token =  (req.header('X-Access-Token')) || '';
-//     // let token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
-//
-//     auth.decodeToken(token, (err, payload) => {
-//         if (err) {
-//             // Invalid token
-//             const error = new ApiError(err.message || err, 401);
-//             console.log(error);
-//             next();
-//         } else {
-//             console.log('Authenticated! Payload = ');
-//             console.dir(payload);
-//             req.user = payload.sub;
-//             next();
-//         }
-//     })
-// };
-
 let validateToken = (req, res, next) => {
     console.log("Validate token called");
     const bearerHeader = req.headers["authorization"];
